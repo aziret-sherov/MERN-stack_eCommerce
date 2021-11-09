81cfe53dfadf90c0f099d323b193446434852a39
aziret-sherov <aziret.sherov@gmail.com>
2021-11-09 22:54:53 +0600

    Implementing Redux For State Management (30-Lesson)

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -6,21 +6,21 @@ const Rating = ({ value, text, color }) => {
     <div className={"rating"}>
       <span>
         <i
-    style={{color}}
-    className={
-        value > 1
-            ? "fas fa-star"
-            : value >= 0.5
-                ? "fas fa-star-half-alt"
-                : "far fa-star"
-    }
-    />
+          style={{ color }}
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        />
       </span>
       <span>
         <i
           style={{ color }}
           className={
-            value > 2
+            value >= 2
               ? "fas fa-star"
               : value >= 1.5
               ? "fas fa-star-half-alt"
@@ -32,7 +32,7 @@ const Rating = ({ value, text, color }) => {
         <i
           style={{ color }}
           className={
-            value > 3
+            value >= 3
               ? "fas fa-star"
               : value >= 2.5
               ? "fas fa-star-half-alt"
@@ -44,7 +44,7 @@ const Rating = ({ value, text, color }) => {
         <i
           style={{ color }}
           className={
-            value > 4
+            value >= 4
               ? "fas fa-star"
               : value >= 3.5
               ? "fas fa-star-half-alt"
@@ -56,7 +56,7 @@ const Rating = ({ value, text, color }) => {
         <i
           style={{ color }}
           className={
-            value > 5
+            value >= 5
               ? "fas fa-star"
               : value >= 4.5
               ? "fas fa-star-half-alt"
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,71 +1,91 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
+import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import {useDispatch, useSelector} from "react-redux";
+import {listProductDetails} from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
-const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
-  return (
-    <>
-      <Link className={"btn btn-light my-3"} to={"/"}>
-        Go back
-      </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={`${product.image}`} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
+const ProductScreen = ({match}) => {
+    const disPatch = useDispatch();
+
+    const productDetails = useSelector((state) => state.productDetails);
+    const {error, loading, product} = productDetails;
+
+    useEffect(() => {
+        disPatch(listProductDetails(match.params.id));
+    }, [disPatch, match]);
+
+    return (
+        <>
+            <Link className={"btn btn-light my-3"} to={"/"}>
+                Go back
+            </Link>
+            {loading ? (
+                <Loader/>
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : (
                 <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
+                    <Col md={6}>
+                        <Image src={`${product.image}`} alt={product.name} fluid/>
+                    </Col>
+                    <Col md={3}>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h3>{product.name}</h3>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Rating
+                                    value={product.rating}
+                                    //   value={5}
+                                    text={`${product.numReview} reviews`}
+                                />
+                            </ListGroup.Item>
+                            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                            <ListGroup.Item>
+                                Description: {product.description}
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col md={3}>
+                        <Card>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Price:</Col>
+                                        <Col>
+                                            <strong>${product.price}</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status:</Col>
+                                        <Col>
+                                            <strong>
+                                                {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                                            </strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button
+                                        className='btn-block'
+                                        type='button'
+                                        disabled={product.countInStock <= 0}
+                                    >
+                                        Add to Card
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
                 </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={product.countInStock <= 0}
-                >
-                  Add to Card
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  );
+            )}
+        </>
+    );
 };
 
 export default ProductScreen;
